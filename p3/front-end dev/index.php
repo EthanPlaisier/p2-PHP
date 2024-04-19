934c3c13f0ac062ab3598e76131b49dc58a2d0c9
Ethan <9018008@student.zadkine.nl>
2024-04-19 16:42:16 +0200

    opdr 3

---
+++ p3/front-end dev/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="topbar">
+        <h1 class="header1">Ethan Plaisier</h1>
+    </header>
+    <main>
+        <section class="fotoarea">
+            <div class="profileimage">
+                <img src="pasfoto_20.jpg" alt="Your Picture" class="image">
+            </div>
+            <div class="aboutmetext">
+                <h5>Kort over mij</h5>
+                <p class="text">Hello there, Ethan here. I work as a Front-end developer. You can find out more below where i talk about myself in more detail.</p>
+            </div>       
+        </section>
+
+        <section class="buttons">
+            <h4>More details</h4>
+            <a href="#" class="button">About me</a>
+            <a href="#" class="button">Contact</a>
+            <a href="#" class="button">Work</a>
+        </section>
+    </main>
+    <footer class="bottombar">
+        <h1>Gemaakt door Ethan</h1>
+    </footer>
+</body>
+</html>
